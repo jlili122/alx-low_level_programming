05711a4cfbc7d0ab32e9dd5bd8e46164d32e1499
jlili <jlilichouaib48@gmail.com>
2023-09-15 16:40:00 -0700

    H

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - prints a square of x-height and x-width (basically a square)
+ * with hashtags.
+ * @size: size of the square that should be printed
+ *
+ * Return: Nothing (always)
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
