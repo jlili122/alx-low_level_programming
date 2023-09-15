ca91edb13166d877774761d086457b477adab573
jlili <jlilichouaib48@gmail.com>
2023-09-15 16:24:59 -0700

    H

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ * _isupper - checks if the passed argument is uppercase or not.
+ * @c: Integer
+ *
+ * Return: 1 if uppercase
+ * 0 if not
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
